9048c9c186d906125f9139b32a71e720bf8c444b
keithmchd48 <keithmchd48@gmail.com>
2024-05-01 18:42:49 +0200

    so gs path

--- src/data/songs.js
+++ src/data/songs.js
@@ -1,11 +1,11 @@
 export const COVER_PATH = 'src/assets/covers'
-let SONG_PATH = 'src/data/assets/songs'
+let SONG_PATH = 'src/assets/songs'
 
 export const songs = [
   {
     id: 0,
     name: 'A window to the past',
-    path: './data/songs/Song1.mp3',
+    path: new URL(`${SONG_PATH}/Song1.mp3`, window.location.href).href,
     artist: 'John Williams',
     cover: new URL(`${COVER_PATH}/cover1.jpeg`, window.location.href).href,
     alt: 'Cover 1'
@@ -13,7 +13,7 @@ export const songs = [
   {
     id: 1,
     name: 'Come on Eileen',
-    path: './data/songs/Song2.mp3',
+    path: new URL(`${SONG_PATH}/Song2.mp3`, window.location.href).href,
     artist: 'Dexys midnight runners',
     cover: new URL(`${COVER_PATH}/cover2.jpg`, window.location.href).href,
     alt: 'Cover 2'
@@ -21,7 +21,7 @@ export const songs = [
   {
     id: 2,
     name: 'Como los olivos',
-    path: './data/songs/Song3.mp3',
+    path: new URL(`${SONG_PATH}/Song3.mp3`, window.location.href).href,
     artist: 'Bebe',
     cover: new URL(`${COVER_PATH}/cover3.jpg`, window.location.href).href,
     alt: 'Cover 3'
@@ -29,7 +29,7 @@ export const songs = [
   {
     id: 3,
     name: 'Heroes',
-    path: './data/songs/Song4.mp3',
+    path: new URL(`${SONG_PATH}/Song4.mp3`, window.location.href).href,
     artist: 'David Bowie',
     cover: new URL(`${COVER_PATH}/cover4.png`, window.location.href).href,
     alt: 'Cover 4'
@@ -37,7 +37,7 @@ export const songs = [
   {
     id: 4,
     name: 'Nobody but me',
-    path: './data/songs/Song5.mp3',
+    path: new URL(`${SONG_PATH}/Song5.mp3`, window.location.href).href,
     artist: 'The Human Beinz',
     cover: new URL(`${COVER_PATH}/cover5.jpeg`, window.location.href).href,
     alt: 'Cover 5'
@@ -45,7 +45,7 @@ export const songs = [
   {
     id: 5,
     name: 'O Children',
-    path: './data/songs/Song6.mp3',
+    path: new URL(`${SONG_PATH}/Song6.mp3`, window.location.href).href,
     artist: 'Nick Cave & the bad seeds',
     cover: new URL(`${COVER_PATH}/cover6.jpeg`, window.location.href).href,
     alt: 'Cover 6'
@@ -53,7 +53,7 @@ export const songs = [
   {
     id: 6,
     name: 'Siempre me quedara',
-    path: './data/songs/Song7.mp3',
+    path: new URL(`${SONG_PATH}/Song7.mp3`, window.location.href).href,
     artist: 'Bebe',
     cover: new URL(`${COVER_PATH}/cover7.jpeg`, window.location.href).href,
     alt: 'Cover 7'
@@ -61,7 +61,7 @@ export const songs = [
   {
     id: 7,
     name: 'The other side',
-    path: './data/songs/Song8.mp3',
+    path: new URL(`${SONG_PATH}/Song8.mp3`, window.location.href).href,
     artist: 'Hugh Jackman & Zac Efron',
     cover: new URL(`${COVER_PATH}/cover8.jpeg`, window.location.href).href,
     alt: 'Cover 8'
@@ -69,7 +69,7 @@ export const songs = [
   {
     id: 8,
     name: 'Femme like you',
-    path: './data/songs/Song9.mp3',
+    path: new URL(`${SONG_PATH}/Song9.mp3`, window.location.href).href,
     artist: 'Monaldin feat. emma peters',
     cover: new URL(`${COVER_PATH}/cover9.jpeg`, window.location.href).href,
     alt: 'Cover 9'
@@ -77,7 +77,7 @@ export const songs = [
   {
     id: 9,
     name: 'Honeypie',
-    path: './data/songs/Song10.mp3',
+    path: new URL(`${SONG_PATH}/Song10.mp3`, window.location.href).href,
     artist: 'JAWNY',
     cover: new URL(`${COVER_PATH}/cover10.jpeg`, window.location.href).href,
     alt: 'Cover 10'
@@ -85,7 +85,7 @@ export const songs = [
   {
     id: 10,
     name: 'Rolling in the deep',
-    path: './data/songs/Song11.mp3',
+    path: new URL(`${SONG_PATH}/Song11.mp3`, window.location.href).href,
     artist: 'Adele',
     cover: new URL(`${COVER_PATH}/cover11.png`, window.location.href).href,
     alt: 'Cover 11'
