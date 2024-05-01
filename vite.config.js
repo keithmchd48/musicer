381fb7fe36e706d58c92cd1599f5b9c2b3739dc9
keithmchd48 <keithmchd48@gmail.com>
2024-05-01 19:30:16 +0200

    vite copy plugin

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,24 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import VueDevTools from 'vite-plugin-vue-devtools'
+import { viteStaticCopy } from 'vite-plugin-static-copy'
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), VueDevTools()],
+  plugins: [
+    vue(),
+    VueDevTools(),
+    viteStaticCopy({
+      targets: [
+        {
+          // eslint-disable-next-line no-undef
+          src: path.resolve(__dirname, './src/assets'),
+          dest: './src'
+        }
+      ]
+    })
+  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
